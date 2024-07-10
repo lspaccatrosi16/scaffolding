6b5bed5be1711a992fd4562b6602571e19b62f08
Luca Spaccatrosi <l.spaccatrosi@outlook.com>
2024-07-10 08:27:40 +0100

    flesh out component

---
+++ ts-web/src/hello.ts
@@ -0,0 +1,20 @@
+import Component from "./component"
+
+class HelloComponent extends Component {
+	constructor() {
+		super();
+	}
+	
+	connectedCallback() {
+		const shadowRoot = this.attachShadow(mode: "closed");
+		const template = document.createElement("template");
+		template.innerHTML = `<h1>Hello, World!</h1>`;
+		shadowRoot.appendChild(template);
+	}
+
+}
+
+customElements.define("hello-component", Component);
+
+export {};
+
